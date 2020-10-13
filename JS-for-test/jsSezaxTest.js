e51d909cdcd3d1b0bf0c9e35385f39cea254cb23
VB <>
2020-10-13 19:28:46 +0700

    create function filter array

--- JS-for-test/jsSezaxTest.js
+++ JS-for-test/jsSezaxTest.js
@@ -5,12 +5,24 @@
 
 const array = [1, 2, 2, 3, 4, 6, 7, 8, 6];
 
-const result = Array.from(new Set(array));
-console.log(result);
+const result1 = Array.from(new Set(array));
+console.log(result1);
 
 const anotherResult = array.filter((item, index) => {
   return array.indexOf(item) === index;
 });
 console.log(anotherResult);
+
+//----------------------------------------
+
+const array = [1, 2, 2, 3, 4, 6, 7, 8, 6];
+
+function name(array) {
+  const result = array.filter((item, index) => array.indexOf(item) === index);
+  return result;
+}
+
+const result = name(array);
+console.log(result);
 
 // ---------------------------------------
